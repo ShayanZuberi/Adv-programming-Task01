768a01d333f90b24f9442d93cb19a35d2809d252
ShayanZuberi <i190749@nu.edu.pk>
2021-09-12 11:20:32 +0500

    Initial Commit

---
+++ firstMaven/src/main/java/firstMaven/HelloWorld.java
@@ -0,0 +1,8 @@
+package firstMaven;
+
+public class HelloWorld {
+	public static void main(String[] arg) {
+		System.out.println("Hello World by Shayan Amir Zuberi");
+	}
+
+}
---
+++ firstMaven/src/main/java/firstMaven/HelloWorld2.java
@@ -0,0 +1,7 @@
+package firstMaven;
+
+public class HelloWorld2 {
+	public static void main(String[] arg) {
+		System.out.println("Hello World2 by Shayan Amir Zuberi");
+	}
+}
