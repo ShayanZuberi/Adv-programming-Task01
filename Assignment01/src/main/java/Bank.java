e81297efb63fc4c1613daedfbee5820a07afdf08
ShayanZuberi <i190749@nu.edu.pk>
2021-09-13 22:29:15 +0500

    initial commit

---
+++ Assignment01/src/main/java/Bank.java
@@ -0,0 +1,79 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Bank {
+	private ArrayList<Customer> customers;
+	private int customerCount;
+	public Bank() {
+		customers = new ArrayList<Customer>();
+		customerCount = 0;
+	}
+	
+	public void addCustomer() {
+		Scanner scan2 = new Scanner(System.in);
+		System.out.println("Please Enter customer name");
+		String name = scan2.nextLine();
+		System.out.println("Please Enter customer's address");
+		String address = scan2.nextLine();
+		System.out.println("Please Enter customer's phone");
+		String phone = scan2.nextLine();
+	    scan2.close();
+	    
+	    
+	    Customer temp = new Customer(name, address, phone);
+	    temp.openAccount();
+	    customers.add(temp);
+	    customerCount++;
+	}
+	
+	
+	public void makeTransaction() {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Please Enter sender's name");
+		String sender = scan.nextLine();
+		System.out.println("Please Enter receiver's name");
+		String receiver = scan.nextLine();
+		int x=-1, y=-1;
+		for (int i=0; i<customerCount; i++) {
+			if (customers.get(i).getName() == sender) {
+				x = i;	//saving sender index
+			}
+			if (customers.get(i).getName() == receiver) {
+				y = i;	//saving receiver index
+			}
+		}
+		
+		if (x == -1 ){
+			System.out.println("Invalid sender name");
+			scan.close();
+			return;
+		}
+		else if (y == -1 ){
+			System.out.println("Invalid receiver name");
+			scan.close();
+			return;
+		}
+		else{
+			System.out.println("Please enter amount to send");
+			float amount  = scan.nextFloat();
+			System.out.println("Press 1 to send from savings accout");
+			System.out.println("Press 2 to send from checking accout");
+			int type  = scan.nextInt();
+			if(customers.get(x).withdraw(type, amount) == true) {
+				customers.get(y).deposit(type, amount);
+			}
+			
+			
+		}
+		scan.close();
+	}
+	
+	public void setInterestRate(float rate) {	//for all
+		for (int i=0; i<customers.size(); i++) {
+			if (customers.get(i).hasSavings() == true) {
+				customers.get(i).setInterest(rate);
+			}
+		}
+	}
+	
+}
---
+++ Assignment01/src/main/java/Main.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+    	Scanner scan = new Scanner(System.in);
+    	Bank bank = new Bank();
+    	String username = "ShayanZuberi";
+    	String password = "banking101";
+    	boolean loginSuccess = false;
+    	
+    	while(loginSuccess == false) {
+    		System.out.println("Please enter admin username: ");
+    		String un = scan.next();
+    		System.out.println("Please enter admin password: ");
+    		String pw = scan.next();
+    		if (un.equals(username) && pw.equals(password)) {
+    			loginSuccess = true;
+    		}
+    		else {
+    			System.out.println("\nIncorrect Credentials ");
+    			System.out.println("Press 1 to try again ");
+    			System.out.println("Press 2 to exit ");
+        		int choice = scan.nextInt();
+        		if (choice == 2) {
+        			break;
+        		}
+    		}
+    	}
+    	
+    	while(loginSuccess == true) {
+    		System.out.println("\nPress 1 to add a new customer");
+    		System.out.println("Press 2 to make a transaction");
+    		System.out.println("Press 3 to set Interest rate for all savings accounts");
+    		System.out.println("Press 4 to");
+    		System.out.println("Press 5 to");
+    		System.out.println("Press 6 to exit");
+    		int choice = scan.nextInt();
+    		if (choice == 1) {
+    			bank.addCustomer();
+    		}
+    		else if (choice == 2) {
+    			bank.makeTransaction();
+    		}
+    		else if (choice == 3) {
+    			System.out.println("\nPlease enter new Interest Rate");
+    			float rate = scan.nextFloat();
+    			bank.setInterestRate(rate);
+    		}
+    		else if (choice == 4) {
+    			
+    		}
+    		else if (choice == 5) {
+    			
+    		}
+    		else if (choice == 6) {
+    			break;
+    		}
+    		
+    		
+    	}
+    	scan.close();
+    }
+
+}
